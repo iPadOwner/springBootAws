8b11e368e23bbf8aadf15934145de66d6f5ea2d9
iPadOwner <zenith21c@naver.com>
2022-04-15 00:11:46 +0900

    oauth 수정중

--- src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -9,6 +9,7 @@ public class HelloResponseDtoTest{
     public void 롬복_기능_테스트(){
         String name = "test";
         int amount = 1000;
+
         HelloResponseDto dto = new HelloResponseDto(name, amount);
 
         assertThat(dto.getName()).isEqualTo(name);
