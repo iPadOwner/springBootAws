6a2b079168ead07b3e0c92141a23074ad8dfcaaa
iPadOwner <zenith21c@naver.com>
2022-04-25 22:23:45 +0900

    jpa 설정 수정

--- src/main/java/com/jojoldu/book/springboot/config/JpaConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/JpaConfig.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @Configuration//@WebMvcTest는 일반적인 @Configuration 는 스캔하지않음
 @EnableJpaAuditing//Application.java에 있던것을 여기로 옮김
 public class JpaConfig {
-
 }
